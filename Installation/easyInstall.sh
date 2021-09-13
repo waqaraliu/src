b12f8cd698eac6085493463deb6aff6d51a53e72
Waqar Ali <goraya.waqar@gmail.com>
2021-09-14 01:05:13 +0500

    Update easyInstall.sh

--- Installation/easyInstall.sh
+++ Installation/easyInstall.sh
@@ -8,5 +8,5 @@ echo "Done installing pip !"
 sudo pip3 install flask
 sudo pip3 install keyboard
 echo "Done installing flask !"
-sudo chmod +x /home/pi/src/launcher.sh
+sudo chmod +x /home/pi/src/Installation/launcher.sh
 echo "All Done!"
